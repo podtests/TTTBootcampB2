92b28d225132eae145af16568dbfe163dd68d437
Akhil Jain <akhiljda@gmail.com>
2024-06-23 09:28:40 +0530

    day1

---
+++ src/main/java/Demo.java
@@ -0,0 +1,41 @@
+public class Demo {
+
+    public static void main(String[] args){
+
+        //Integer family
+        byte d = 4;
+
+        short e = 18;
+
+        int a = 2147483647;
+
+        long f = 567;
+
+        int age = 89;
+
+
+        //float
+        float b = 1.23F;
+
+        double c = 1.23;
+
+
+        //boolean
+        boolean g = true;  //false
+
+
+        //char
+        char characterName = 'A';
+
+
+        //String family
+        String name = "Akhil Jain";
+
+
+        System.out.println(name);
+
+    }
+
+
+
+}
