a8ca726ded711d4f04c6f100a78b150c3f6438c2
Akhil Jain <akhiljda@gmail.com>
2024-09-15 11:03:58 +0530

    resolve cucumber errors2

--- src/test/java/in/podtest/cucumber/runners/CucumberTestRunner.java
+++ src/test/java/in/podtest/cucumber/runners/CucumberTestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/resources/features/Login.feature"},
         glue = {"in.podtest.cucumber.stepdefinitions", "in.podtest.cucumber.hooks"},
         tags =  "@jira100",
-        plugin = {"html:target/cucumber.html"}
+        plugin = {"html:target/cucumber.html", "json:target/cucumber.json"}
 )
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
